9c8c1919a6259bc564ce9a1ccb57318c48f639e3
Andrew <andrew.am2017@gmail.com>
2022-07-11 22:13:23 +0300

    настроил роуты и страницы, подключил статику

--- app/my_crm/urls.py
+++ app/my_crm/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index-page'),
+    path('<slug:stage_slug>', views.index, name='index-stage'),
+    path('client/', views.client, name='client-page'),
+    path('settings/', views.settings, name='index-settings'),
+    path('statistics/', views.statistics, name='index-statistics'),
 ]
--- app/my_crm/views.py
+++ app/my_crm/views.py
@@ -4,6 +4,16 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'base.html', {})
+    return render(request, 'my_crm/index.html', {})
 
+def stage(request, stage_slug):
+    return render(request, 'my_crm/index.html', {})
 
+def client(request):
+    return render(request, 'my_crm/client.html', {})
+
+def settings(request):
+    return render(request, 'my_crm/settings.html', {})
+
+def statistics(request):
+    return render(request, 'my_crm/statistics.html', {})
